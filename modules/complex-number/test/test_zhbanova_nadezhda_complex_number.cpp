515487bb740c6fa2c31165631d8d04547007507a
Надя <>
2021-03-25 12:22:56 +0300

    no tabs

--- modules/complex-number/test/test_zhbanova_nadezhda_complex_number.cpp
+++ modules/complex-number/test/test_zhbanova_nadezhda_complex_number.cpp
@@ -5,30 +5,30 @@
 #include "include/complex_number.h"
 
 TEST(Nadezhda_Zhbanova_ComplexNumberTest, divide_by_zero) {
-	ComplexNumber z1;
-	ComplexNumber z2(z1);
+    ComplexNumber z1;
+    ComplexNumber z2(z1);
 
-	EXPECT_ANY_THROW(z2 / z1);
+    EXPECT_ANY_THROW(z2 / z1);
 }
 
 TEST(Nadezhda_Zhbanova_ComplexNumberTest, divide_the_number_by_itself) {
-	ComplexNumber z1(3.0, 7.0);
-	ComplexNumber z2;
-	double re = 3.0;
-	double im = 7.0;
-	z2.setRe(re);
-	z2.setIm(im);
-	ComplexNumber z = z2 / z1;
+    ComplexNumber z1(3.0, 7.0);
+    ComplexNumber z2;
+    double re = 3.0;
+    double im = 7.0;
+    z2.setRe(re);
+    z2.setIm(im);
+    ComplexNumber z = z2 / z1;
 
-	ComplexNumber expected_z(1.0, 0.0);
-	EXPECT_EQ(z, expected_z);
+    ComplexNumber expected_z(1.0, 0.0);
+    EXPECT_EQ(z, expected_z);
 }
 
 TEST(Nadezhda_Zhbanova_ComplexNumberTest, difference_and_get_re_get_im) {
-	ComplexNumber z1(3.0, 7.0);
-	ComplexNumber z2(6.0, 6.0);
-	ComplexNumber z = z2 - z1;
+    ComplexNumber z1(3.0, 7.0);
+    ComplexNumber z2(6.0, 6.0);
+    ComplexNumber z = z2 - z1;
 
-	EXPECT_EQ(3.0, z.getRe());
-	EXPECT_EQ(-1.0, z.getIm());
+    EXPECT_EQ(3.0, z.getRe());
+    EXPECT_EQ(-1.0, z.getIm());
 }
